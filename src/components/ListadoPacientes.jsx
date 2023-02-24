858b9314046d42f8e92ba06b0a73fff5bc067523
Rafael <ralexmejia27@gmail.com>
2023-02-24 12:37:55 -0400

    first commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import React from 'react'
+import Paciente from './Paciente'
+
+function ListadoPacientes({ pacientes, setPaciente, eliminarPaciente }) {
+
+  useEffect(() => {
+    if(pacientes.length > 0){
+        console.log("nuevo paciente")
+    }
+  }, [pacientes])
+
+  return (
+
+    <div className='md:w-1/2 lg:w3/5 md:h-screen overflow-y-scroll'>
+
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Administra tus {''}
+            <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+          </p>
+
+          { pacientes.map( paciente => (
+
+                <Paciente
+                  key={paciente.id}
+                  paciente={paciente}
+                  setPaciente={setPaciente}
+                  eliminarPaciente={eliminarPaciente}
+                />
+
+            )
+          )}
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center'>No hay pacientes</h2>
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Comienza agregando pacientes {''}
+            <span className='text-indigo-600 font-bold'>Y se enlistaran aqui</span>
+          </p>
+        </>
+      )}
+
+      
+
+    </div>
+
+  )
+
+}
+
+export default ListadoPacientes
